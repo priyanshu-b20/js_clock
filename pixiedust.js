6c9bc53386762ff7903b786adb9ca38c75d40e86
hunny <priyanshu.bisht20@gmail.com>
2020-03-03 19:58:30 +0530

    made clock double digits

--- pixiedust.js
+++ pixiedust.js
@@ -1,7 +1,15 @@
 var clk = document.getElementById('clock');
+var mm = document.getElementById('mm');
 setInterval(clock,1000);
 function clock(){
+	function r(n){
+		if(n<10){
+			return "0"+n;
+		}
+		else{
+			return n;
+		}
+	}
 	var d = new Date();
-	clk.innerHTML = ((d.getHours()>12) ? d.getHours()-12 : d.getHours()) + " : " + d.getMinutes(); //+ " " + ((d.getHours()>12) ? 'PM' : 'AM');
+	clk.innerHTML = ((d.getHours()>12) ? r(d.getHours()-12) : r(d.getHours())) + " : " + r(d.getMinutes()) + ' ' + ((d.getHours()>12) ? 'PM' : 'AM');
 }
-
