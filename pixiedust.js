d46bbd3817e725c2ef839118a353a66c3c5a2848
hunny <priyanshu.bisht20@gmail.com>
2020-04-12 22:44:30 +0530

    only js

--- pixiedust.js
+++ pixiedust.js
@@ -1,7 +1,9 @@
 var things = document.getElementById('things');
 var mm = document.getElementById('mm');
-setInterval(clock,1000);
 function clock(){
+	setInterval(clk,1000);
+}
+function clk(){
 	function r(n){
 		if(n<10){
 			return "0"+n;
@@ -12,4 +14,23 @@ function clock(){
 	}
 	var d = new Date();
 	things.innerHTML = ((d.getHours()>12) ? r(d.getHours()-12) : r(d.getHours())) + " : " + r(d.getMinutes()) + ' ' + ((d.getHours()>12) ? 'PM' : 'AM');
+}
+function stopwatch(){
+	var e = document.getElementById('sw');
+	
+	setInterval(sw,1000);
+}
+var i=0,j=0,k=0;
+function sw(){
+	things.innerHTML = i + " : " + j + " : " + k;
+	k++;
+	if(k==60){
+		k=0;
+		j++
+	}
+	
+	if(j==60){
+		j=0;
+		i++;
+	}
 }
