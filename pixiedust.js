09897ad4637509fa4741dd982863484ee2bf0ec2
hunny <priyanshu.bisht20@gmail.com>
2020-04-13 05:05:36 +0530

    working on stopwatch

--- pixiedust.js
+++ pixiedust.js
@@ -1,18 +1,29 @@
-let clk = null
+let clk,sw,tm
+let min = 0
+let sec = 0
 
+const cls = () => {
+	clearInterval(sw)
+	clearInterval(clk)
+	clearInterval(tm)
+}
 const clock = () => {
+	cls()
+	$('input[value="stopwatch"]').show()
+	$('input[value="lap"]').hide()
+	$('input[value="stop"]').hide()
 	clk = setInterval(() => {
 		const date = new Date()
 		$('#things').text(`${date.getHours()} : ${date.getMinutes()}`)
 	},1000)
 	
 }
-const stopwatch = () => {
-	let min = 0
-	let sec = 0
-	console.log('hello')
-	clearInterval(clk)
-	setInterval(() => {
+const stopwatch = (minx=0,secx=0) => {
+	cls()
+	$('input[value="stopwatch"]').hide()
+	$('input[value="lap"]').show()
+	$('input[value="stop"]').show()
+	sw = setInterval(() => {
 		$('#things').text(`${min} : ${sec}`)
 		sec++
 		if(sec==60){
@@ -23,11 +34,38 @@ const stopwatch = () => {
 	},1000)
 	
 }
-
+const lap = () => {
+	
+}
+const stop = () => {
+	clearInterval(sw)
+	$('input[value="stop"]').hide()
+	$('input[value="start"]').show()
+}
+const start = () => {
+	$('input[value="stop"]').show()
+	$('input[value="start"]').hide()	
+	console.log(min+" "+sec)
+	stopwatch()
+}
+const timer = () => {
+	$('input[value="stopwatch"]').show()
+	$('input[value="lap"]').hide()
+	$('input[value="stop"]').hide()	
+}
+const about = () => {
+	$('input[value="stopwatch"]').show()
+	$('input[value="lap"]').hide()
+	$('input[value="stop"]').hide()	
+}
 $(document).ready(() =>{
 	clock()
-	$('input[value="stopwatch"]').click(() => clock())
+	$('input[value="clock"]').click(() => clock())
 	$('input[value="stopwatch"]').click(() => stopwatch())
-
+	$('input[value="timer"]').click(() => timer())
+	$('input[value="about"]').click(() => about())
+	$('input[value="lap"]').click(() => lap())
+	$('input[value="stop"]').click(() => stop())
+	$('input[value="start"]').click(() => start())
 });
 
